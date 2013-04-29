eb5625d211b37fd272ae5f65c3e699f672098e1a
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-04-29 13:25:39 +0200

    work.. ua. handling instanzierungen

--- __pca__Instrument.js
+++ __pca__Instrument.js
@@ -1,43 +1,100 @@
+/* Instrument Userscript Snippet: function(){ ...
+
+    if(__pca__) {
+        var __pca__liner = __pca__.liner(this, arguments);
+        if(__pca__liner) return __pca__liner;
+    };
+*/
+
 (function(window, document, undefined){
     function __pca__Instrument() {
         if (this == window) return new __pca__Instrument(); // has to be called as Constructor
-        this.index = 0;
-        this.maxDepth = 3;
-        this.dontInstrument = ["document", "location", "localStorage","sessionStorage", "debug", "__pca__", "__pca__objInfo"];
+/*//////////////////////////////////////////////////////////////////////////////
+    Private Properties
+/*//////////////////////////////////////////////////////////////////////////////
+        var objIndex = 0;
+        var maxDepth = 3;
+        var dontInstrument = ["document", "location", "localStorage","sessionStorage", "debug", "__pca__", "__pca__objInfo"];
 
-        this.send = function (data) {
+        var structure = {};
+/*//////////////////////////////////////////////////////////////////////////////
+    Public Properties
+/*//////////////////////////////////////////////////////////////////////////////
+/*//////////////////////////////////////////////////////////////////////////////
+    Private Methods
+/*//////////////////////////////////////////////////////////////////////////////
+        var getPropertyType = function(property){
+            return property!=null && typeof(property.__pca__objInfo) == "object"?  
+                property.__pca__objInfo.oid:
+                    property == null?
+                      "null":
+                      typeof(property); 
+        };
+ ///////////////////////////////////////////////////////////////////////////////
+       var getObjId = function(method){
+            return method.__pca__objInfo && method.__pca__objInfo.oid >=0 ?
+                method.__pca__objInfo.oid:
+                -1;
+        }
+ ///////////////////////////////////////////////////////////////////////////////
+       var getProperties = function(method){
+            var properties = {};
+            for(var m in method) 
+                if(m!="__pca__objInfo") 
+                   properties[m] = getPropertyType(method[m]);
+               
+            return properties;
+        }
+///////////////////////////////////////////////////////////////////////////////       
+        var instrumentation = function (_this, _arguments) {
+            var info = _this.__pca__objInfo;
+            sendData({
+                oid : info.oid,
+                name : info.name,
+                properties : getProperties(info.obj)
+            });
+        }
+///////////////////////////////////////////////////////////////////////////////
+        var sendData = function (data) {
             console.debug(data);
         }
-        this.wrapper = function (method, depth) {
-            console.debug(depth);
+/*//////////////////////////////////////////////////////////////////////////////
+    Public Methods
+/*//////////////////////////////////////////////////////////////////////////////
+        this.wrapper = function (method, depth, parent, name) {
             switch(typeof(method)) {
                 case "function" :
-                    var newMethod = function() {
-                        if (this.__pca__objInfo == undefined) __pca__.wrapper(this);
-                        __pca__.instrumentation(this, arguments);
-                        return __pca__.wrapper(method.apply(this, arguments));
+                    var wrapper = function() {
+                        var result, tempArguments;
+                        if(getObjId(this) < 0 && this != window) { // if new instance/ constructor
+                            tempArguments = Array.prototype.slice.call(arguments);
+                            Array.prototype.unshift.call(tempArguments, null);
+                            result = new (Function.prototype.bind.apply(method, tempArguments)); 
+                            result = __pca__.wrapper(result);
+                            instrumentation(result, arguments);
+                        }  
+                        else { // if normal function call
+                            result = __pca__.wrapper(method.apply(this, arguments));
+                            instrumentation(this, arguments);
+                        }
+                        return result;
                     };
-                    return method.toString() == newMethod.toString() ?
+                    return method.toString() == wrapper.toString() ?
                         method:
-                        newMethod;
+                        wrapper;
                 break;            
                 case  "object":
-                    if(method == null || depth <= 0) return method;
-                    
-                    var properties=this.getProperties(method);
 
-                    if(!method.__pca__objInfo || !method.__pca__objInfo.properties) method.__pca__objInfo={properties:[]};                      
-
-                    if(properties.length == method.__pca__objInfo.properties.length) return method;
-                    else method.__pca__objInfo = {
-                        oid:this.index++, 
-                        name: method.constructor.name, 
+                    if(method == null || depth > maxDepth || (getObjId(method) >= 0 && depth != undefined)) return method;
+                 
+                    method.__pca__objInfo = {
+                        oid:objIndex++, 
+                        name: method.constructor.name != "" ? method.constructor.name : name, 
                         obj: method
                     };
-
                     for(var m in method){
-                        if(this.dontInstrument.indexOf(m) < 0) {
-                            method[m]=this.wrapper(method[m], depth-1 || this.maxDepth);
+                        if(dontInstrument.indexOf(m) < 0) {
+                            method[m]=this.wrapper(method[m], depth+1 || 0, method, m);
                         }
                     }
                 default:
@@ -45,30 +102,27 @@
                 break;   
             }
         }
-        this.getProperties = function(method){
-            var properties = {};
-            for(var m in method) 
-                if(m!="__pca__objInfo") {
-                    properties[m] =  method[m]!=null && typeof(method[m].__pca__objInfo) == "object"?  
-                        method[m].__pca__objInfo.oid:
-                            method[m] == null?
-                              "null":
-                              typeof(method[m]); 
-                }
-            return properties;
-        }
+ ///////////////////////////////////////////////////////////////////////////////
         this.liner = function (_this, _arguments) {
-            if (_arguments.callee.caller != null && _arguments.callee.caller.toString() == this.wrapper().toString()) return; //if wrapped
-            this.instrumentation(_this, _arguments);
+            var newClass, tempArguments;
+            if(_arguments.callee.caller != null && _arguments.callee.caller.toString() == this.wrapper(this.liner).toString()) return; //if wrapped
+            
+            if(_this != window && getObjId(_this) < 0 && _arguments[_arguments.length-1] != "__pca__new") {
+                var tempArguments = Array.prototype.slice.call(_arguments);
+                Array.prototype.push.call(tempArguments, "__pca__new");
+                Array.prototype.unshift.call(tempArguments, null);
+                var newClass = new (Function.prototype.bind.apply(_arguments.callee, tempArguments));
+
+                this.wrapper(newClass);
+                instrumentation(newClass, _arguments);
+                return newClass;
+            }
+            else {
+                Array.prototype.splice.call(_arguments, _arguments.length-1, 1);
+                return false;
+            }
         }
-        this.instrumentation = function (_this, _arguments) {
-            var info = _this.__pca__objInfo;
-            this.send({
-                oid : info.oid,
-                name : info.name,
-                properties : this.getProperties(info.obj)
-            });
-        }
+ ///////////////////////////////////////////////////////////////////////////////
     }
     __pca__=__pca__Instrument();
     __pca__.wrapper(window);
@@ -76,7 +130,6 @@
 //tested in http://jsfiddle.net/SuJwg/16/
 
 /*
-window = __pca__.wrapper(window);
 //__pca__.liner(this, arguments);
 
 function myClass() {
