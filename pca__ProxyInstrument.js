495b34105d92f2a058064d9a9b70eee55b54d2f4
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-04-30 21:46:06 +0200

    Works pretty awesomely :D

--- pca__ProxyInstrument.js
+++ pca__ProxyInstrument.js
@@ -1,4 +1,3 @@
-
 (function(window, document, undefined){
 	function __pca__Instrument() {
 			if (this == window) return new __pca__Instrument(); // has to be called as Constructor
@@ -8,7 +7,8 @@
 			var objIndex = 0;
 			var maxDepth=3;
 			var doInstrument = ["object", "function"];
-			var dontInstrument = [document, window.location, window.console.debug, "__pca__", this.wrapper, this.liner, window.chrome, null, "prototype"];
+			var dontInstrument = [document, window.location, "Proxy", "Reflect", "indexOf", console, "console", "__pca__", window.chrome, null, "prototype"];
+			var windowProxied=false;
 /*//////////////////////////////////////////////////////////////////////////////
 	Public Properties
 /*//////////////////////////////////////////////////////////////////////////////
@@ -23,26 +23,28 @@
 	Public Methods
 /*//////////////////////////////////////////////////////////////////////////////
 			this.wrapper = function (obj, depth, parent, name) {
-				console.log(obj);
-				if(dontInstrument.indexOf(obj) >= 0 || dontInstrument.indexOf(name) >= 0 || doInstrument.indexOf(typeof(obj)) < 0 || depth <= 0) return obj;
+				if(dontInstrument.indexOf(obj) >= 0 || dontInstrument.indexOf(name) >= 0 || doInstrument.indexOf(typeof(obj)) < 0 || depth <= 1 || (obj == window && windowProxied)) return obj;
+				
+				if(obj == window) windowProxied=true;
 				var doIt;
 
 				for(var p in obj){
-					console.log(p);
 					try{
-						doIt = dontInstrument.indexOf(p) < 0 && dontInstrument.indexOf(obj[p]) <0 && this.doInstrument.indexOf(typeof(obj[p])) >=0 ?
+						doIt = dontInstrument.indexOf(p) < 0 && dontInstrument.indexOf(obj[p]) <0 && doInstrument.indexOf(typeof(obj[p])) >=0 ?
 							true:false;
-					} catch(e){ doIt=false; console.debug(e);};
+					} catch(e){ doIt=false; console.debug(e);}
 					if(doIt) obj[p]=this.wrapper(obj[p], (depth >=0 ? depth-1 : maxDepth) , obj, p);
 				}
-
-				return Proxy(obj, {
+//console.debug("__________________________________________________________","proxied",obj.__pca__Proxied, objIndex);
+				
+				if(obj.__pca__Proxied) return obj;
+				else return Proxy(obj, {
 					get: function(obj, name) {
 						if(name == "__pca__objInfo") return this.__pca__objInfo;
 						if(name == "__pca__objIndex") return this.__pca__objIndex;
 						if(name == "__pca__Proxied") return true;
 
-						console.log("Proxie","get", this.__pca__objIndex);
+						//console.log("Proxie","get", this.__pca__objIndex);
 						return Reflect.get(obj, name);
 					},
 					set: function(obj, name, value) {
@@ -50,17 +52,16 @@
 							this.__pca__objInfo=value;
 							return;
 						}
-
-						console.log("Proxie","set", this.__pca__objIndex);
+					//	console.log("Proxie","set", this.__pca__objIndex);
 						return Reflect.set(obj, name, value);
 					}, 
 					apply: function(method, _this, arguments) {
-						console.log("Proxie","apply", this.__pca__objIndex);
+						console.debug("Proxie","apply", this.__pca__objIndex);
 						return Reflect.apply(method, _this, arguments);
 					},
 					construct: function(method, arguments) {
-						console.log("Proxie","construct", this.__pca__objIndex);
-						return Reflect.construct(method, arguments);
+						console.debug("Proxie","construct", this.__pca__objIndex);
+						return __pca__.wrapper(Reflect.construct(method, arguments));
 					},
 					__pca__objIndex:objIndex++
 				});
