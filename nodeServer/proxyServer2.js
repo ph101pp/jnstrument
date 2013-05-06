c573ee33c5e80b9a52eb5c2f31057b932a97c90c
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-05-07 01:23:48 +0200

    Proxy seems to work

--- nodeServer/proxyServer2.js
+++ nodeServer/proxyServer2.js
@@ -1,18 +1,19 @@
-    var httpProxy = require('http-proxy');
-    var url = require('url');
-     
-    httpProxy.createServer(function(req, res, proxy) {
-     
-      var urlObj = url.parse(req.url);
-      
-      req.headers['host'] = urlObj.host;
-      req.headers['url'] = urlObj.href;
-     
-      proxy.proxyRequest(req, res, {
-        host: urlObj.host,
-        port: 80,
-        enable : { xforward: true }
-      });
-    }).listen(9000, function () {
-      console.log("Waiting for requests...");
-    });
+var httpProxy = require('http-proxy');
+var url       = require('url');
+
+httpProxy.createServer(function(req, res, proxy) {
+  var urlObj = url.parse(req.url);
+
+  req.headers.host  = urlObj.host;
+  req.url           = urlObj.path;
+
+  console.log(urlObj.host);
+
+  proxy.proxyRequest(req, res, {
+    host    : urlObj.host,
+    port    : 80,
+    enable  : { xforward: true }
+  });
+}).listen(9000, function () {
+  console.log("Waiting for requests...");
+});
