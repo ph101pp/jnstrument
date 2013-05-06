c0b87203415ef5c90d98ff6aee53f5f714292acc
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-05-06 23:29:06 +0200

    Clean up ChromePlugin.. Deleting old stuff

--- chromePlugin/browserAction.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(
-  		null, 
-  		{
-  			code:"document.write('\<script\> alert(\"red\"); \</script\>'"
-  		}
-  );
-});
