19779ba189f2134b1f641d6b7916494d803c4696
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-05-07 18:16:40 +0200

    First Website with node, express and stylus

--- app/app.js
+++ app/app.js
@@ -1,8 +1,21 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware({ 
+	src: __dirname + '/public', 
+	compile: function(str, path){
+	  return stylus(str).set('filename', path).use(require('nib')());
+	}
+}))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',{ 
+  	title : 'Home' 
+  });
+})
 app.listen(3000);
-console.log('Listening on port 3000');
