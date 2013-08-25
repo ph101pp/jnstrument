0a47bb956af1a6a16359b779347fc75384969cf9
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-08-25 17:29:02 +0200

    Adding example links to chrome extension

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -1,43 +1,60 @@
 chrome.tabs.query({currentWindow:true, active:true}, function(result){
-	var tab=result[0];
-	var bgPage=chrome.extension.getBackgroundPage();
-	var activateLink=document.getElementById("activate");
-	var deactivateLink=document.getElementById("deactivate");
-	var gotoLink=document.getElementById("open");
-	var https;
-	var checkLink = function(){
-		if(!window.Proxy) {
-			document.body.setAttribute("class", "flags");
-			return;
-		}
-		else if(tab.url.match(/^https/)) {
-			document.body.setAttribute("class", "https");
-			return;
-		}
-		var status = bgPage.jnstrument.tabStatus(tab);
-		if(status && status.status) {
-			document.body.setAttribute("class", "active");
-		}
-		else {
-			document.body.setAttribute("class", "deactive");
-		}
-	}
-	var openVisualization = function(){
-		var status = bgPage.jnstrument.tabStatus(tab);
-		if(!status || !status.status) return;
-		chrome.windows.create({'url': "http://jnstrument.com/"+status.guid+"/neuron"});
-	}
-	var activate = function(){
-		bgPage.jnstrument.activate(tab);
-		checkLink();
-		openVisualization();
-	}
+	var tab=result[0],
+		bgPage=chrome.extension.getBackgroundPage(),
+		activateLink=document.getElementById("activate"),
+		deactivateLink=document.getElementById("deactivate"),
+		google=document.getElementById("visualizeGoogle"),
+		daum=document.getElementById("visualizeDaum"),
+		naver=document.getElementById("visualizeNaver"),
+		gotoLink=document.getElementById("open"),
+		https,
+		checkLink = function(){
+			if(!window.Proxy) {
+				document.body.setAttribute("class", "flags");
+				return;
+			}
+			else if(tab.url.match(/^https/)) {
+				document.body.setAttribute("class", "https");
+				return;
+			}
+			var status = bgPage.jnstrument.tabStatus(tab);
+			if(status && status.status) {
+				document.body.setAttribute("class", "active");
+			}
+			else {
+				document.body.setAttribute("class", "deactive");
+			}
+		},
+		openVisualization = function(){
+			var status = bgPage.jnstrument.tabStatus(tab);
+			if(!status || !status.status) return;
+			chrome.windows.create({'url': "http://jnstrument.com/"+status.guid+"/neuron"});
+		},
+		activate = function(){
+			bgPage.jnstrument.activate(tab);
+			checkLink();
+			openVisualization();
+		},
+		visualize = function(url){
+			chrome.tabs.create({url:url}, function(_tab){
+				tab=_tab;
+				activate();
+			});
+		};
 
 	checkLink();
 
 	activateLink.addEventListener("click", activate);
 	deactivateLink.addEventListener("click", activate);
-
+	google.addEventListener("click", function(){
+		visualize("http://www.google.com");
+	});
+	daum.addEventListener("click", function(){
+		visualize("http://www.daum.net");
+	});
+	naver.addEventListener("click", function(){
+		visualize("http://www.naver.com");
+	});
 	gotoLink.addEventListener("click", function(){
 		openVisualization();
 	});
