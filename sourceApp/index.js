5c761fb07b185db1b8a14c085094aaa33e3cdcbf
Philipp Adrian <hello@philippadrian.com>
2017-07-15 20:10:07 +0000

    rename test/ to sourceApp/ and ignore .log files

--- sourceApp/index.js
+++ sourceApp/index.js
@@ -1,19 +1,14 @@
 const _ = require('./lodash');
 
-
 var object = {
-  'a': 1
+  'a': 1,
+  'c': {
+      d: 1
+    }
 };
  
 var other = {
   'b': 2
 };
-
-function test() {
-  return 'hello';  
-}
-function tests() {
-}
-
 
 console.log(_.merge(object, other));
